93b96c5e28179e678c6af49fb4ebd4e29c2ff6a7
David Ben-Yaakov <david@tangogolfdigital.com>
2020-03-19 22:39:18 -0500

    Updated for new build system

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
              targets: ["Curses"]),
   ], 
   dependencies: [
-    .package(url:  "https://github.com/TangoGolfDigital/CNCURSES.git", from: "1.0.0"),
+    .package(url:  "https://github.com/TheCoderMerlin/CNCURSES.git", from: "1.0.0"),
   ],
   targets: [
     .target(
